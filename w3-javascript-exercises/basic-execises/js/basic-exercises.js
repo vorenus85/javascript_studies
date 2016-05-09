4c6ac7f97cb0cdca655d71cbffedede579ec805b
vorenus85 <janos.perge@gmail.com>
2016-05-09 22:02:17 +0200

    refakt + w3resource gyakorlatok

---
+++ w3-javascript-exercises/basic-execises/js/basic-exercises.js
@@ -0,0 +1,15 @@
+/**
+ * Created by János on 2016.05.09..
+ */
+
+var date = new Date();
+var day = date.getDay();
+var days = ["Sunday", "Monday", "Thuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+console.log("Today is : " + days[day]);
+
+
+//h:MM:ss TT --> 5:46:21 PM
+console.log("\n");
+
+var now = new Date('YYYY');
+console.log(now);
