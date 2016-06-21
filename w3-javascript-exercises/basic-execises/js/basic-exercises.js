9286704b3e0c2421e1423c3b557f41d8f551bcfa
vorenus85 <janos.perge@gmail.com>
2016-06-21 18:35:04 +0200

    exercise 1.1, 1.2, 1.3, 1.4

--- w3-javascript-exercises/basic-execises/js/basic-exercises.js
+++ w3-javascript-exercises/basic-execises/js/basic-exercises.js
@@ -2,14 +2,73 @@
  * Created by János on 2016.05.09..
  */
 
-var date = new Date();
-var day = date.getDay();
-var days = ["Sunday", "Monday", "Thuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-console.log("Today is : " + days[day]);
+function AMPM(hour){
+    var ampm ="";
+    if(hour <= 12 ){
+        ampm = "AM";
+    }else{
+        ampm = "PM";
+    }
+
+    return ampm;
+}
+
+function exercise_1_1(){
+    var date = new Date();
+    var day = date.getDay();
+    var days = ["Sunday", "Monday", "Thuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 
-//h:MM:ss TT --> 5:46:21 PM
-console.log("\n");
+    //h:MM:ss TT --> 5:46:21 PM
 
-var now = new Date('YYYY');
-console.log(now);
+    var toConsole = "Today is : " + days[day] + "\n";
+
+    var hours = date.getHours();
+    var ampm = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+
+    if(hours >= 12){
+        hours = hours - 12;
+    }
+
+    toConsole += 'Current time is : ' + hours + ' ' + AMPM(ampm) + ' : ' + minute + ' : ' + second;
+
+    return console.log(toConsole);
+}
+
+function exercise_1_2(){
+    window.print();
+}
+
+function exercise_1_3(){
+    var date = new Date();
+    var yyyy = date.getFullYear();
+    var mm = date.getMonth() + 1;
+    mm = '0' + mm;
+    var dd = '0' + date.getDate();
+
+    mm = mm.slice(-2);
+    dd = dd.slice(-2);
+
+    //mm-dd-yyyy
+    console.log(mm + '-' + dd + '-' + yyyy+'\n');
+
+    //dd-mm-yyyy
+    console.log(dd + '-' + mm + '-' + yyyy+'\n');
+
+}
+
+function exercise_1_4(side1, side2, side3){
+    /* need Heron  formula*/
+    var halfPerimater = (side1 + side2 + side3) / 2;
+    var underRooth = halfPerimater * (halfPerimater - side1) * (halfPerimater - side2) * (halfPerimater - side3);
+    var triangleArea = Math.sqrt(underRooth);
+
+    return console.log(triangleArea);
+}
+
+exercise_1_1();
+//exercise_1_2();
+exercise_1_3();
+exercise_1_4(5,6,7);
